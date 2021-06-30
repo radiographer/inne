12a804b56b22d74c45073f0038884026cf6a97cd
radiographer <lukas.w@gmx.com>
2021-06-30 18:59:49 +0200

    pagination

---
+++ rick-and-morty-app/src/App.js
@@ -0,0 +1,68 @@
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import Character from "./components/Character";
+import Pagination from "./components/Pagination";
+
+function App(props) {
+  const [loading, setLoading] = useState(true);
+  const [characters, setCharacters] = useState([]);
+  const [currentPageUrl, setCurrentPageUrl] = useState(
+    "https://rickandmortyapi.com/api/character"
+  );
+  const [nextPageUrl, setNextPageUrl] = useState();
+  const [prevPageUrl, setPrevPageUrl] = useState();
+  const [pages, setPages] = useState();
+
+  useEffect(() => {
+    const url = currentPageUrl;
+    setLoading(true);
+    const fetchData = async () => {
+      const res = await fetch(url);
+      const data = await res.json();
+      setCharacters(data.results);
+      setLoading(false);
+      setNextPageUrl(data.info.next);
+      setPrevPageUrl(data.info.prev);
+      setPages(data.info.pages);
+    };
+    fetchData();
+  }, [currentPageUrl]);
+
+  function nextPage() {
+    setCurrentPageUrl(nextPageUrl);
+  }
+  function prevPage() {
+    setCurrentPageUrl(prevPageUrl);
+  }
+  function goToPage(num) {
+    setCurrentPageUrl(`https://rickandmortyapi.com/api/character?page=${num}`);
+  }
+  if (loading) return "Loading...";
+  const charList = characters.map((char) => (
+    <Character
+      key={Math.floor(Math.random() * 10000)}
+      name={char.name}
+      img={char.image}
+    />
+  ));
+
+  return (
+    <div className="App">
+      <Pagination
+        nextPage={nextPageUrl ? nextPage : null}
+        prevPage={prevPageUrl ? prevPage : null}
+        goToPage={goToPage}
+        pages={pages}
+      />
+      <div className="char-cards">{charList}</div>
+      <Pagination
+        nextPage={nextPageUrl ? nextPage : null}
+        prevPage={prevPageUrl ? prevPage : null}
+        goToPage={goToPage}
+        pages={pages}
+      />
+    </div>
+  );
+}
+
+export default App;
